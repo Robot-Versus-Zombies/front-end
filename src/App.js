bbddf1c6b73a7a2e3b75eb335dd3a71cfca50314
JossWritesCode <joscelynrose@gmail.com>
2020-08-24 15:14:25 -0400

    added a header and title and centered things

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import GameBoard from './components/GameBoard';
+import Header from './components/Header';
+
 import './SCSS/main.scss';
 
 function App() {
 	return (
 		<div className="App">
+			<Header />
+			<h1 className="title">Robot vs Zombies</h1>
 			<GameBoard />
 		</div>
 	);
