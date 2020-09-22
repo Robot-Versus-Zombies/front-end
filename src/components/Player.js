e39419b3d7039eccef22dbe5c3fef25b06335091
JossWritesCode <joscelynrose@gmail.com>
2020-09-21 20:58:30 -0400

    fixed scrolling issue
    Co-authored-by: paxfeline <paxfeline@gmail.com>

--- src/components/Player.js
+++ src/components/Player.js
@@ -4,9 +4,7 @@ function Player({ playerX, playerY }) {
 	return (
 		<div
 			style={{ top: playerY * 42 + 'px', left: playerX * 42 + 'px' }}
-			className="player">
-			<div className="player-boundary"></div>
-		</div>
+			className="player"></div>
 	);
 }
 
