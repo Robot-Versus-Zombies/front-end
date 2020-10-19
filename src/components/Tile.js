f656174d021acce0deed6c92e7d885ebb0f7c27d
Devin Warrick <dev.warr@gmail.com>
2020-10-19 00:28:38 -0500

    Creates indoor tiles and places inside buildings

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { WallTile } from '../utils/tileClass';
+import { BuildingTile, WallTile } from '../utils/tileClass';
 
 function Tile({ tileData }) {
 	let cssClasses = 'tile ';
 	if (tileData instanceof WallTile) cssClasses += 'wall ';
+	if (tileData instanceof BuildingTile) cssClasses += 'floor ';
 
 	return <div className={cssClasses}>{}</div>;
 }
