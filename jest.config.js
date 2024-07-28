cd9e920383e08813c0dcb2262c1f9d1c4de436b1
JossWritesCode <joscelynrose@gmail.com>
2024-07-28 16:44:55 -0400

    chore: implemented first test file

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
 	preset: 'ts-jest',
-	testEnvironment: 'node',
+	testEnvironment: 'jsdom',
 	moduleNameMapper: {
 		'\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',
 	},
 	transform: {
-		'^.+.tsx?$': ['ts-jest', {}],
+		'^.+\\.tsx?$': ['ts-jest', {}],
 	},
 };
--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,5 +1,6 @@
+import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
-import React from 'react';
+import * as React from 'react';
 
 import Header from './Header';
 
@@ -8,10 +9,7 @@ describe('Header Component', () => {
 		const { getByAltText } = render(<Header />);
 		const logo = getByAltText('rvz logo');
 		expect(logo).toBeInTheDocument();
-		expect(logo).toHaveAttribute(
-			'src',
-			'../../images/just-text-rvz-png.png',
-		);
+		expect(logo).toHaveAttribute('src', 'test-file-stub');
 		expect(logo).toHaveAttribute('width', '100px');
 	});
 });
