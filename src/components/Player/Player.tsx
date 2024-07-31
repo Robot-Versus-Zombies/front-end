7304813f5cfcf7e10f350d5735483518a3d6f5eb
JossWritesCode <joscelynrose@gmail.com>
2024-07-30 22:05:20 -0400

    chore: added tests for the Player component

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 
+import { Directions } from '../../gameLogic/helpers/directions';
+
 type Props = {
 	playerX: number;
 	playerY: number;
-	direction: 0 | 1 | 2 | 3;
+	direction?: Directions; // Add a question mark (?) to make it optional
 };
 
-const Player = ({ playerX, playerY, direction }: Props) => {
-	return (
-		<div
-			style={{ top: playerY * 42 + 'px', left: playerX * 42 + 'px' }}
-			className={`player player-${direction}`}
-		/>
-	);
+const cellSize = 42;
+
+const Player: React.FC<Props> = ({
+	playerX,
+	playerY,
+	direction = 2, // Set the default value to 2
+}: Props): JSX.Element => {
+	const style: React.CSSProperties = {
+		top: playerY * cellSize + 'px',
+		left: playerX * cellSize + 'px',
+	};
+
+	return <div style={style} className={`player player-${direction}`} />;
 };
 
 export default Player;
