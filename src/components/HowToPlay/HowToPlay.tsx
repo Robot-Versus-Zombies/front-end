8a6e87d88b96643317aa0b2a7e8b0fb6d05a1399
JossWritesCode <joscelynrose@gmail.com>
2024-08-01 10:35:37 -0400

    feat: there is now a how-to-play section

---
+++ src/components/HowToPlay/HowToPlay.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HowToPlay: React.FC = () => {
+	return (
+		<div className="how-to-play">
+			<h2>How to Play</h2>
+			<h3>WASD to Move</h3>
+		</div>
+	);
+};
+
+export default HowToPlay;
